e1955f212bbacc69571cab1cf9dc244ce57daff5
Pedro Mendonça <pgcalixto@gmail.com>
2017-10-25 10:37:11 -0200

    Change points to list instead of 2 Int

--- Mastermind.hs
+++ Mastermind.hs
@@ -12,20 +12,20 @@ This is just the player.
 
 module Mastermind
 ( arrangements
-, points
+, getPoints
 , updatePool
 ) where
 
 import Data.List (nub)
 
--- | Obtains all possible arrangements of [1,2,3,4,5,6] with a length of 4
+-- | Obtains all possible arrangements of [1,2,3,4,5,6] with a length of 4.
 arrangements :: [[Int]]
 arrangements = filter ((==4) . length) $
                map (nub) [x | x <- mapM (const [1,2,3,4,5,6]) [1..4]]
 
--- | Returns correct and regular points of the guess, given the answer
-points :: Eq a => [a] -> [a] -> [Int]
-points answer guess =
+-- | Returns correct and regular points of the guess, given the answer.
+getPoints :: Eq a => [a] -> [a] -> [Int]
+getPoints answer guess =
   [c, r]
   where c = length $ filter id $ zipWith (==) answer guess
         r = (length . filter (`elem` answer) $ guess) - c
@@ -34,9 +34,9 @@ points answer guess =
   Updates the pool given an existent pool, the last guess and its points,
   and returns the updated pool.
 -}
-updatePool :: [[Int]] -> [Int] -> Int -> Int -> [[Int]]
-updatePool [] _ _ _ = []
-updatePool (poolH:poolT) guess correct regular =
-    if (poolH /= guess && points guess poolH == [correct, regular]) then
-      poolH : updatePool poolT guess correct regular
-    else updatePool poolT guess correct regular
+updatePool :: [[Int]] -> [Int] -> [Int] -> [[Int]]
+updatePool [] _ _ = []
+updatePool (poolH:poolT) guess points =
+    if (poolH /= guess && getPoints guess poolH == points) then
+      poolH : updatePool poolT guess points
+    else updatePool poolT guess points
--- Test.hs
+++ Test.hs
@@ -8,29 +8,29 @@ main = hspec $ do
     it "should have length 1296" $
       length arrangements `shouldBe` 360
 
-  describe "points" $ do
+  describe "getPoints" $ do
     it "returns [4,0] when the guess is correct" $ do
-      points [1,2,3,4] [1,2,3,4] `shouldBe` [4,0]
+      getPoints [1,2,3,4] [1,2,3,4] `shouldBe` [4,0]
 
     it "returns [0,4] when the guess is the reversed answer" $
       let answer = [1,2,3,4]
           guess = reverse answer
-      in do points answer guess `shouldBe` [0,4]
+      in do getPoints answer guess `shouldBe` [0,4]
 
     it "returns [2,2] when then guess has 2 corrects and 2 regulars" $
       let answer = [4,5,6,1]
           guess  = [6,5,4,1]
-      in do points answer guess `shouldBe` [2,2]
+      in do getPoints answer guess `shouldBe` [2,2]
 
     it "returns [0,3] when then guess has 1 correct and 3 regulars" $
       let answer = [5,1,6,3]
           guess  = [2,5,3,1]
-      in do points answer guess `shouldBe` [0,3]
+      in do getPoints answer guess `shouldBe` [0,3]
 
   describe "pool" $ do
 
     it "returns the desired new pool" $
       let pool = arrangements
-      in do (updatePool pool [1,2,3,4] 0 4)
+      in do (updatePool pool [1,2,3,4] [0,4])
         `shouldBe` [[2,1,4,3], [2,3,4,1], [2,4,1,3], [3,1,4,2], [3,4,1,2],
                     [3,4,2,1], [4,1,2,3], [4,3,1,2], [4,3,2,1]]
